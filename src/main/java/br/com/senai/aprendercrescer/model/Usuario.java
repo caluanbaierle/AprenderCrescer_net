517270665fb638f01fb23c11716ad8f8d855cf73
Caluan Baierle <caluan.baierle@gmail.com>
2016-09-12 20:25:44 -0300

    comit  parcial

---
+++ src/main/java/br/com/senai/aprendercrescer/model/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.senai.aprendercrescer.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Caluan Baierle
+ */
+public class Usuario {
+    private int idusuario;
+    private int idgrupo;
+    private String login;
+    private String senha;
+    private String nome;
+    private char flagInativo;
+    private Date dtAlteracao;
+}
