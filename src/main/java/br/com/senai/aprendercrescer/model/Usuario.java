156693f67ec60d657e4117f24c59d41cf0a3b230
Caluan Baierle <caluan.baierle@gmail.com>
2016-09-15 21:53:35 -0300

    save arezzo files

--- src/main/java/br/com/senai/aprendercrescer/model/Usuario.java
+++ src/main/java/br/com/senai/aprendercrescer/model/Usuario.java
@@ -12,11 +12,73 @@ import java.util.Date;
  * @author Caluan Baierle
  */
 public class Usuario {
-    private int idusuario;
-    private int idgrupo;
+    private int idUsuario;
+    private int idGrupo;
     private String login;
     private String senha;
     private String nome;
     private char flagInativo;
     private Date dtAlteracao;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdGrupo() {
+        return idGrupo;
+    }
+
+    public void setIdGrupo(int idGrupo) {
+        this.idGrupo = idGrupo;
+    }
+
+    
+    
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getFlagInativo() {
+        return flagInativo;
+    }
+
+    public void setFlagInativo(char flagInativo) {
+        this.flagInativo = flagInativo;
+    }
+
+    public Date getDtAlteracao() {
+        return dtAlteracao;
+    }
+
+    public void setDtAlteracao(Date dtAlteracao) {
+        this.dtAlteracao = dtAlteracao;
+    }
+    
+    
+    
 }
